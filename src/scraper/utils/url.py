fe00597e6119919112a9b86c01d00d8c4d97b6e2
MingotJuliette <mingotjuliette.contact@gmail.com>
2026-02-15 02:23:49 +0100

    Create url.py

---
+++ src/scraper/utils/url.py
@@ -0,0 +1,9 @@
+import os
+
+BASE = "https://www.federalreserve.gov"
+HISTORICAL = BASE + "/monetarypolicy/fomc_historical_year.htm"
+CALENDAR = BASE + "/monetarypolicy/fomccalendars.htm"
+
+SLEEP = 0.4
+
+# https://www.federalreserve.gov/newsevents/pressreleases/monetary20170201a.htm
